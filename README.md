6fa63be3bf56af7750c9e0284a16d3f94abb0f5e
liyalong <1223118063@qq.com>
2025-01-16 08:33:39 +0800

    增加记录视频进度功能

--- README.md
+++ README.md
@@ -1,8 +1,14 @@
 
 ## edge浏览器bilibili插件
+https://microsoftedge.microsoft.com/addons/detail/bilbili-%E8%A7%86%E9%A2%91%E5%80%8D%E9%80%9Fshift19-%E9%A6%96/deckncdcckinokobbkifppdcdeacdojl
 
-- [x] 使用 Shift + 1 至 Shift + 9 快捷键调节视频播放速度。
+- [x] 使用 Shift + 1 至 Shift + 9 快捷键调节视频播放速度, shift + ~ 调节速度到0.5。
 - [x] 去除 Bilibili 首页的大屏元素。
 - [x] 实现 Bilibili 首页的换一换回溯功能。
+- [x] 快捷键ctrl+/ 记录当前视频播放进度。
+- [x] 快捷键B 视频进度回到ctrl+/记录过的播放进度。
 - [ ] ......
 
+## 使用方法
+1. 打成压缩包
+2. 拖到浏览器扩展
--- scripts/stepless-video-rate.js
+++ scripts/stepless-video-rate.js
@@ -2,31 +2,63 @@
  * 键盘倍速shift 1~9
  */
 document.addEventListener('keydown', function (event) {
-  let video = document.querySelector('video');
+    let video = document.querySelector('video');
 
-  if (event.shiftKey) {
-    switch (event.key) {
-      case '#':
-        video.playbackRate = 3;
-        break;
-      case '$':
-        video.playbackRate = 4;
-        break;
-      case '%':
-        video.playbackRate = 5;
-        break;
-      case '^':
-        video.playbackRate = 6;
-        break;
-      case '&':
-        video.playbackRate = 7
-        break;
-      case '*':
-        video.playbackRate = 8;
-        break;
-      case '(':
-        video.playbackRate = 9;
-        break;
+    if (event.shiftKey) {
+        switch (event.key) {
+            case '~':
+                video.playbackRate = 0.5;
+                break;
+            case '!':
+                video.playbackRate = 1;
+                break;
+            case '@':
+                video.playbackRate = 2;
+                break;
+            case '#':
+                video.playbackRate = 3;
+                break;
+            case '$':
+                video.playbackRate = 4;
+                break;
+            case '%':
+                video.playbackRate = 5;
+                break;
+            case '^':
+                video.playbackRate = 6;
+                break;
+            case '&':
+                video.playbackRate = 7
+                break;
+            case '*':
+                video.playbackRate = 8;
+                break;
+            case '(':
+                video.playbackRate = 9;
+                break;
+        }
     }
-  }
+
+    // 复制视频时间到剪贴板
+    if (event.ctrlKey) {
+        switch (event.key) {
+            case '/':
+                const baseUrl = window.location.origin + window.location.pathname;
+                navigator.clipboard.writeText(baseUrl + "?t=" + video.currentTime)
+                window.videoCurrentTime = video.currentTime
+                break;
+        }
+    }
+
+    // 回到具体时间
+    if (event.key === 'b' || event.key === 'B') {
+        if (video) {
+            if (window.videoCurrentTime) {
+                video.currentTime = videoCurrentTime
+            } else {
+                video.currentTime = 0
+            }
+        }
+    }
 });
+
